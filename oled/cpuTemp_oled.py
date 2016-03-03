ffd1d9ca04402b855b4853f31388190e96429d86
luisdiazugena <luis.diaz@bq.com>
2016-03-03 16:50:17 +0100

    Working on a display for the cpu temperature using an oled display

---
+++ oled/cpuTemp_oled.py
@@ -0,0 +1,32 @@
+from time import sleep
+
+import sys
+from subprocess import call
+import commands
+import os
+
+from oled.device import ssd1306, sh1106
+from oled.render import canvas
+from PIL import ImageFont
+
+font = ImageFont.load_default()
+oled = ssd1306(port=1, address=0x3C)
+
+def getTempCPU():
+    temp = commands.getoutput("/opt/vc/bin/vcgencmd measure_temp")
+    initTempPos = str(temp).find("=")
+    finishTempPos = str(temp).find("'")
+    temp = str(temp)[initTempPos+1:finishTempPos]
+    try:
+        temp_num = float(temp)
+        return temp_num
+    except:
+        print "Unable to transform to float"
+
+with canvas(oled) as draw:
+    padding = 2
+
+    font = ImageFont.load_default()
+    cpuTemp = getTempCPU()
+
+    draw.text((x,x),cpuTemp,font=font,fill=255)
