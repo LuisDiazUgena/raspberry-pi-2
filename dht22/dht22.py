e21cdb3f590c7adbd1c7417825aaa8247869b07a
luisdiazugena <luisdiazugena@gmail.com>
2016-03-11 13:04:30 +0100

    fixed error

--- dht22/dht22.py
+++ dht22/dht22.py
@@ -2,6 +2,7 @@
 import Adafruit_DHT
 
 sensor = Adafruit_DHT.DHT22
+pin=21
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
